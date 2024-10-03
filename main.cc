8e1ca49cb55dc70442da957978130bdd7bc499fb
Qwerty7310 <daniyl.2004@yandex.ru>
2024-10-03 22:28:23 +0300

    cd

---
+++ main.cc
@@ -0,0 +1,48 @@
+#include <limits.h>  // Для PATH_MAX
+#include <unistd.h>  // Для getcwd
+
+#include <iostream>
+
+int main() {
+    char cwd[PATH_MAX];  // Буфер для хранения пути
+
+    // Получаем текущую директорию
+    if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+        std::cout << "Текущая директория: " << cwd << std::endl;
+    } else {
+        perror("getcwd() ошибка");
+    }
+
+    return 0;
+}
+
+/* ------------- FORK --------------- */
+
+/*
+#include <sys/wait.h>  // Для wait
+#include <unistd.h>  // Для fork и exec
+
+#include <iostream>
+
+int main() {
+    pid_t pid = fork();  // Создаём дочерний процесс
+
+    if (pid == -1) {
+        // Ошибка при создании дочернего процесса
+        perror("fork failed");
+        return 1;
+    } else if (pid == 0) {
+        // Это дочерний процесс
+        std::cout << "Executing ls in child process..." << std::endl;
+        execl("/bin/ls", "ls", NULL);  // Замещаем текущий процесс программой ls
+        perror("execl failed");  // Если exec не сработал, выводим ошибку
+        _exit(1);
+    } else {
+        // Это родительский процесс
+        wait(NULL);  // Ожидаем завершения дочернего процесса
+        std::cout << "Child process finished." << std::endl;
+    }
+
+    return 0;
+}
+*/
--- terminal.cc
+++ terminal.cc
@@ -1,1 +1,88 @@
+#include <limits.h>  // Для PATH_MAX
+#include <unistd.h>
+
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
+
+#define RESET "\033[0m"
+#define RED "\033[1;31m"
+#define YELLOW "\033[1;33m"
+#define WHITE "\033[1;37m"
+#define GREEN "\033[1;32m"
+#define ORANGE "\033[38;5;214m"
+
+#define BUF_SIZE 1024
+
+#define OK 0
+#define ERROR 1
+
+using namespace std;
+
+int changeDir(string s, int len_s, char *cur_dir, string home_dir);
+
+int main() {
+    printf("%sStart terminal...%s\n", ORANGE, RESET);
+
+    string home_dir = getenv("HOME");  // Получаем путь к домашней директории
+
+    if ((home_dir.c_str() == nullptr) || (chdir(home_dir.c_str()) != 0)) perror("chdir() error");
+
+    char cur_dir[PATH_MAX];  // Буфер для хранения пути
+
+    while (true) {
+        if (getcwd(cur_dir, sizeof(cur_dir)) != nullptr)
+            printf("%s%s%s: ", GREEN, cur_dir, RESET);
+        else {
+            perror("getcwd() error");
+            break;
+        }
+
+        string s = "";
+
+        getline(cin, s);
+        int len_s = strlen(s.c_str());
+        if (len_s == 0) continue;
+
+        string command = strtok((char *)s.c_str(), " ");
+
+        if (command == "exit") {
+            break;
+        } else if (command == "cd") {
+            changeDir(s, len_s, cur_dir, home_dir);
+            // if (len_s > 2) {
+            //     string path = s.substr(strlen(command.c_str()) + 1, len_s);
+            //     if (chdir(path.c_str()) == 0) {
+            //         strcpy(cur_dir, path.c_str());
+            //     } else {
+            //         printf("Такой директории нет\n");
+            //     }
+            // } else {
+            //     if ((chdir(home_dir.c_str()) != 0)) {
+            //         perror("chdir() error");
+            //     } else {
+            //         cout << "Success." << endl;
+            //     }
+            // }
+
+        } else {
+            system(s.c_str());
+        }
+    }
+    printf("%sClose terminal...%s\n", RED, RESET);
+    return 0;
+}
+
+int changeDir(string s, int len_s, char *cur_dir, string home_dir) {
+    if (len_s > 2) {
+		printf("<%ld>\n", strlen("cd"));
+        string path = s.substr(strlen("cd") + 1, len_s);
+        if (chdir(path.c_str()) == 0)
+            strcpy(cur_dir, path.c_str());
+        else
+            printf("cd: no such directory: %s\n", path.c_str());
+    } else if (chdir(home_dir.c_str()) != 0)
+        perror("chdir() error");
+	
+	return 0;
+}
