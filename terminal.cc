b0fb1ff1cbdaf1539110b6327a3732ef52a29930
Qwerty7310 <daniyl.2004@yandex.ru>
2024-10-09 01:05:57 +0300

    update cd again

--- terminal.cc
+++ terminal.cc
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <vector>
 
 #define RESET "\033[0m"
 #define RED "\033[1;31m"
@@ -18,19 +19,16 @@
 #define OK 0
 #define ERROR 1
 
-#define ARGS_SIZE 10;
-
 using namespace std;
 
-int changeDir(string s, int len_s, char *cur_dir, string home_dir);
+int changeDir(vector<string> args, char *cur_dir, string home_dir);
 
 int main() {
     printf("%sStart terminal...%s\n", ORANGE, RESET);
-
     string home_dir = getenv("HOME");  // Получаем путь к домашней директории
-
-    if ((home_dir.c_str() == nullptr) || (chdir(home_dir.c_str()) != 0))
-        perror("chdir() error");  // Переходим домашнюю директорию
+    // Переходим в домашнюю директорию
+    if ((home_dir.c_str() == nullptr) || (chdir(home_dir.c_str()) != 0)) 
+        perror("chdir() error");  
 
     char cur_dir[PATH_MAX];  // Буфер для хранения пути
 
@@ -56,79 +54,71 @@ int main() {
             s.erase(i, 1);
             i -= 1;
         }
+        s = s + " ";  // добавляем пробел для корректой работы strtok при отстутствии аргументов
 
-        int cnt_arg = 0;  // Счетчик аргументов функции
-        int args_size = ARGS_SIZE;
-        string *args = (string *)malloc(args_size * sizeof(string));
+        string command = strtok((char *)s.c_str(), " ");  // получаем команду
+        s[command.size()] = ' '; // меняем '\0' после strtok на пробел
+        string str_args = " " + s.substr(command.size() + 1, s.size()) + " ";  // получаем строку аргументов
 
-        bool flag_single_quot = false;
-        size_t ind_single = 0;
-        bool flag_double_quot = false;
-        size_t ind_double = 0;
+        // удаляем лишние пробелы
+        i = 0;
+        while (i + 1 < str_args.size() && str_args[i + 1] == ' ') str_args.erase(i + 1, 1);
 
-        for (size_t i = 0; i < s.size(); i++)
-            if (s[i] == '\'') {
-                if (!flag_single_quot) {
-                    ind_single = i;
-                } else {
-                    s.erase(i, 1);
-                    s.erase(ind_single, 1);
+        vector<string> args;  // вектор аргументов функции
+
+        bool flag_double_quot = false;  // флаг открытия кавычек
+        size_t ind_space = 0;           // индекс последнего пробела
+
+        // разбиваем строку на отдельные аргументы
+        for (size_t i = 0; i < str_args.size(); i++) {
+            if (str_args[i] == '\\')
+                str_args.erase(i, 1);  // удаляем символ '\', i теперь указывает на экранируемый символ, на
+                                       // следующей итерации i++ перепрыгнет этот экранируемый символ
+            else if (str_args[i] == '"') {
+                flag_double_quot = !flag_double_quot;
+                str_args.erase(i, 1);
+                i -= 1;  // после удаления символы смещаются на 1 назад, необходимо вернуться на 1 символ
+                         // назад, чтобы попасть на следующий символ
+            } else if ((str_args[i] == ' ') && (!flag_double_quot)) {
+                if (i - ind_space > 1) {
+                    args.push_back(str_args.substr(ind_space + 1, i - (ind_space + 1)));
                 }
-                flag_single_quot = !flag_single_quot;
+                ind_space = i;
             }
-            else if (s[i] == '"') {
-                if (!flag_double_quot) {
-                    ind_double = i;
-                } else {
-                    s.erase(i, 1);
-                    s.erase(ind_double, 1);
-                }
-                flag_double_quot = !flag_double_quot; 
-            }
-            else if (s[i] == ' ') {
-                if (s[i - 1] != '\\' && !flag_single_quot && !flag_double_quot) cnt_arg += 1;  // Считаем аргументы функции
-                while (i + 1 < s.size() && s[i + 1] == ' ') s.erase(i + 1, 1);  // Удаляем лишние пробелы
-            }
+        }
 
-        printf("<%s><%d>\n", s.c_str(), cnt_arg);
+        //если не закрыты кавычки
+        if (flag_double_quot) {
+            printf("%s\b: incorrect use of quotation marks\n", s.c_str());
+            continue;
+        }
 
-        int len_s = s.size();
-        if (len_s == 0) continue;
-
-        string command = strtok((char *)s.c_str(), " ");
-
-        if (command == "exit") {
+        // обрабатываем команды
+        if (command == "close")
             break;
-        } else if (command == "cd") {
-            if (cnt_arg > 1)
+        else if (command == "cd") {
+            if (args.size() > 1)
                 printf("cd: too many arguments\n");
             else
-                changeDir(s, len_s, cur_dir, home_dir);
+                changeDir(args, cur_dir, home_dir);
         } else {
             printf("%s%s%s\n", PINK, command.c_str(), RESET);
             system(s.c_str());
         }
     }
     printf("%sClose terminal...%s\n", RED, RESET);
+
     return 0;
 }
 
-int changeDir(string s, int len_s, char *cur_dir, string home_dir) {
-    if (len_s > 3) {
-        printf("<%ld>\n", strlen("cd"));
-        string path = s.substr(strlen("cd") + 1, len_s);
-
-        // Удаляем лишние '\'
-        for (size_t i = 0; i < path.size(); i++)
-            if (path[i] == '\\') path.erase(i, 1);
-
+int changeDir(vector<string> args, char *cur_dir, string home_dir) {
+    if (!args.empty()) {
         // Переходим в новую директорию
-        printf("%s%s%s\n", YELLOW, path.c_str(), RESET);
-        if (chdir(path.c_str()) == 0)
-            strcpy(cur_dir, path.c_str());  // в случае успеха меняем текущую директорию
+        if (chdir(args[0].c_str()) == 0)
+            strcpy(cur_dir, args[0].c_str());  // в случае успеха меняем текущую директорию
         else
-            printf("cd: no such directory: %s\n", path.c_str()); // сообщение об ошибке
-    } else if (chdir(home_dir.c_str()) != 0) // переходим в домашнюю директорию после команды "cd"
+            printf("cd: no such directory: %s\n", args[0].c_str());  // сообщение об ошибке
+    } else if (chdir(home_dir.c_str()) != 0)  // переходим в домашнюю директорию после команды "cd"
         perror("chdir() error");
 
     return 0;
